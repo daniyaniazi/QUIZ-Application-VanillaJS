1bc90f8d32a8648952b40a9bf1e233ac4aea8a4e
daniyaniazi <farihadania@hotmail.com>
2020-07-26 21:11:04 +0500

    added gifs

--- end.js
+++ end.js
@@ -1,10 +1,12 @@
 let finalScore = document.getElementById('finalscore');
-
+let statusIMG = document.getElementById('status-img');
 // Getlocalstorage;
 const mostRecentScore = localStorage.getItem('mostRecentScore')
 if (mostRecentScore > 10) {
     finalScore.className = 'text-success';
+    statusIMG.src = 'https://animated.name/uploads/posts/2016-08/1472038239_1342.gif';
 } else {
     finalScore.className = 'text-danger';
+    statusIMG.src = 'https://media.giphy.com/media/SqAwfYxt4dG9k0ma68/giphy.gif';
 }
 finalScore.innerText = mostRecentScore;
